51bcdae47ed7ec73c11520d30c6b643683fca20d
Mayankgupta-coder <mynkgpt16@gmail.com>
2021-10-29 23:04:49 +0530

    add title functionality added

--- notes website/index.js
+++ notes website/index.js
@@ -6,15 +6,21 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function(e) {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
+  let title = localStorage.getItem("title");
   if (notes == null) {
     notesObj = [];
+    titleObj = [];
   } else {
     notesObj.push(addTxt.value);
+    titleObj.push(addTitle.value);
   }
   
   localStorage.setItem("notes", JSON.stringify(notesObj));
+  localStorage.setItem("title", JSON.stringify(titleObj));
   addTxt.value = "";
+  addTitle.value="";
 //   console.log(notesObj);
   showNotes();
 });
@@ -22,17 +28,20 @@ addBtn.addEventListener("click", function(e) {
 // Function to show elements from localStorage
 function showNotes() {
   let notes = localStorage.getItem("notes");
+  let title = localStorage.getItem("title");
   if (notes == null) {
     notesObj = [];
+    titleObj = [];
   } else {
     notesObj = JSON.parse(notes);
+    titleObj = JSON.parse(title);
   }
   let html = "";
   notesObj.forEach(function(element, index) {
     html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
+                        <h5 class="card-title">${titleObj[index]}</h5>
                         <p class="card-text"> ${element}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
@@ -51,14 +60,19 @@ function deleteNote(index) {
 //   console.log("I am deleting", index);
 
   let notes = localStorage.getItem("notes");
+  let title = localStorage.getItem("title");
   if (notes == null) {
     notesObj = [];
+    titleObj = [];
   } else {
     notesObj = JSON.parse(notes);
+    titleObj = JSON.parse(title);
   }
 
   notesObj.splice(index, 1);
+  titleObj.splice(index, 1);
   localStorage.setItem("notes", JSON.stringify(notesObj));
+  localStorage.setItem("title", JSON.stringify(titleObj));
   showNotes();
 }
 
